0d0e00a7a2a0f95d224b421d279ff1c1b71979e1
karlieward <112447674+karlieward@users.noreply.github.com>
2025-02-27 15:31:46 -0700

    my function
    renamed/deleted code I didn't need

---
+++ Intro.py
@@ -0,0 +1,9 @@
+
+
+def introduction():
+    print(f"\nWOMEN'S SOCCER SEASON\nHow to play:\nChoose from the menu to begin")
+    iUserName = input("What is your name? ")
+    print()
+    print(f"Welcome {iUserName}! Let's play!")
+
+    return iUserName
