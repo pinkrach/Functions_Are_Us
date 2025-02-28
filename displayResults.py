38d05e28028e909736a2dcd2d106122cde486084
Biggs1791 <bigelspe@byu.edu>
2025-02-27 22:58:50 -0700

    Create displayResults.py

---
+++ displayResults.py
@@ -0,0 +1,7 @@
+# Spencer Bigelow
+# Display the final record for a team. 
+# Receive the home team data and display information.
+
+def displayResult(winningTeamName, record) :
+    print(f"{winningTeamName}'s record is {record}")
+    return 
