cdfd4d272c6b09a642da197c3d8b23e41429789e
Biggs1791 <bigelspe@byu.edu>
2025-02-27 23:07:52 -0700

    Update displayResults.py

--- displayResults.py
+++ displayResults.py
@@ -2,6 +2,15 @@
 # Display the final record for a team. 
 # Receive the home team data and display information.
 
-def displayResult(winningTeamName, record) :
-    print(f"{winningTeamName}'s record is {record}")
+# Making the function that will print out the results and name of home team.
+def displayResult(homeTeam, numWin, numLose) :
+    print(f"{homeTeam} has {numWin} wins and {numLose}")
     return 
+
+# Setting paramaters so i could test the funtion 
+homeTeam = "BYU"
+numWin = 20
+numLose = 4
+
+# Running function 
+displayResult(homeTeam, numWin, numLose)
