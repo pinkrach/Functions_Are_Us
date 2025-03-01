5bcc4a313fce563e2bb0d3b7daca36fcfe6cca34
Zargesm <Zargesm@byu.edu>
2025-03-01 11:39:40 -0700

    Add playGame function to simulate game outcomes

--- Main.py
+++ Main.py
@@ -2,9 +2,10 @@
 # Program takes input on team names, randomly generate scores,
 # reaturns the scores for each game, number of wins vs loses, 
 # and a statment about the season
+import random
 
 from chooseTeam import *
 from Menu import *
 from intro import *
-
+from masonsfunction import *
     
